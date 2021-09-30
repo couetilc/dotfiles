7c157743814271a08b0d36db9e0342f3f9289c54
Connor Couetil <couetilc@gmail.com>
2021-09-29 20:51:43 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 # /bin/bash -c "$(curl -fsSL https://
 git init --bare $HOME/.dotfiles
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+type dotfiles
 dotfiles config status.showUntrackedFiles no
 dotfiles remote add origin git@github.com:couetilc/dotfiles.git
