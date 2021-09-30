37fe848a6545f65b14ed3659cdcafd8d2e22f55d
Connor Couetil <couetilc@gmail.com>
2021-09-29 21:19:49 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,6 +10,9 @@ git init --bare $HOME/.dotfiles
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 dotfiles config status.showUntrackedFiles no
 dotfiles remote add origin git@github.com:couetilc/dotfiles.git
+# TODO this fails because I do not have read permission yet for the origin.
+# I need to login to github's cli. which means downloading homebrew then
+# `brew install gh && gh auth:login` or whatever.
 dotfiles pull origin main
 
 if [ -n "$ZSH_VERSION" ]; then
