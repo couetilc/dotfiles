cd1ef713e61c121a2335eebac834444935cc5285
Connor Couetil <couetilc@gmail.com>
2021-09-29 21:17:15 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,19 @@
-# /bin/bash -c "$(curl -fsSL https://
-git init --bare "$HOME/.dotfiles"
-alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
-type dotfiles
+# To bootstrap these dotfiles on a computer:
+#
+#   echo "$(curl -fsSL https://raw.githubusercontent.com/couetilc/dotfiles/bootstrap/bootstrap.sh)" | source /dev/stdin
+#
+# NOTE: we call to `source` instead of `bash -ic` to have the dotfiles alias
+# defined and the appropriate rc file sourced in the current shell session
+
+git config --global init.defaultBranch 'main'
+git init --bare $HOME/.dotfiles
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 dotfiles config status.showUntrackedFiles no
 dotfiles remote add origin git@github.com:couetilc/dotfiles.git
+dotfiles pull origin main
+
+if [ -n "$ZSH_VERSION" ]; then
+  source $HOME/.zshrc
+elif [ -n "$BASH_VERSION" ]; then
+  source $HOME/.bashrc
+fi
